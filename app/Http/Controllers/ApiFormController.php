d2022d725f3cae6bb99788b940131d94c49ae606
network-service24 <info@network-service.it>
2025-02-19 12:07:05 +0100

    improved code

---
+++ app/Http/Controllers/ApiFormController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use Webklex\IMAP\Client;
+use DateTime;
+
+class ApiFormController extends Controller
+{    
+    
+
+
+
+
+
+
+
+    
+}
